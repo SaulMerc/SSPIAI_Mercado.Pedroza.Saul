e45b7a8489d70acf19ce254e2cade52a061e483f
SaulMerc <saul.mercado9192@alumnos.udg.mx>
2023-09-22 12:08:20 -0600

    Correcciones en el código (nombres de los archivos y métodos)

--- E2.2_Agentes/BcFact.pl
+++ E2.2_Agentes/BcFact.pl
@@ -5,4 +5,9 @@ factorial(5, 120).
 factorial(6, 720).
 factorial(12, 479001600).
 factorial(2, 2).
+factorial(7, 5040).
+factorial(14, 87178291200).
+factorial(18, 6402373705728000).
+factorial(4, 3).
+factorial(13, 6227020800).
 
