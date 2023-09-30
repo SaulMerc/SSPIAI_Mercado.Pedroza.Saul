b0b942f31d99630e76aa30658351ae1a4e1c09fd
SaulMerc <saul.mercado9192@alumnos.udg.mx>
2023-09-29 19:31:03 -0600

    [E2.3] Trabajo hecho en clase, próximo a modificar

---
+++ E2.3_Listas/s14.pl
@@ -0,0 +1,60 @@
+%Listas
+
+%Simple
+mamiferos([leon,perro,tigre,caballo]).
+semana([lunes,martes,miercoles]).
+
+%Listas de listas
+animales(
+    [leon,tigre,jaguar],
+    [caballo],
+    [perro,oso],
+    [oso_homigero]
+).
+
+%Listas como relación
+animales(carnivoro,
+    [lobo,leopardo,leon]
+).
+
+animales(insectivoro,
+    [rana,camaleon]
+).
+%insertar
+insertar(E,L,Lr):- Lr = [E|L].
+insertar1(E,L,[E|L]).
+insertar2(E,L,[Rt|Rh]):-Rh=E, Rt=L.
+
+agregar(E,[],[E]):- !.
+agregar(E,[H|T],[H|L]) :-agregar(E,T,L).
+
+%longitud
+longitud([], 0).
+longitud([_|T], L) :- longitud(T,L1), L is L1 + 1.
+
+
+%buscar
+buscar(B,[B|_]).
+buscar(B,[_|T]):- buscar(B,T).
+
+%Concatenar
+concat([], L, L).
+concat([H|T], L, [H,Result]):- concat(T,L,Result).
+%REmover
+remover(_,[],[]).
+remover(R,[R|T],T2):- remover(R,T,T2).
+remover(R,[H|T],[H|T2]):- H\=R , remover(R,T,T2).
+%Remover primer valor
+remover1(_,[],[]).
+remover1(R,[R|T],T):- !.
+remover1(R,[H|T],[H|T2]):- H\=R , remover1(R,T,T2).
+
+%is_list - Comprobar  
+%member - Buscar
+%append - Concatenar
+%delete - Eliminar
+%sort - Ordenar eliminando duplicados
+%lenght - Longitud
+%msort - Ordenar sin eliminar duplicados
+
+contar_elementos(Tipo,Cant) :- animales(Tipo,Lista),longitud(Lista,Cant).
