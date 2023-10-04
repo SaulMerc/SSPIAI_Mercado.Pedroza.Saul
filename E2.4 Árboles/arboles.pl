d0658c1e48ea7426a307ad0ddef133b1be20452a
SaulMerc <saul.mercado9192@alumnos.udg.mx>
2023-10-03 23:25:40 -0600

    Agregado de los ejercicios solicitados, árboles gráfico actualizados

--- E2.4 Árboles/arboles.pl
+++ E2.4 Árboles/arboles.pl
@@ -14,16 +14,18 @@ arbol(a, arbol("M",arbol(a,"S",arbol(l,u,' ')),arbol(r,e,c)), arbol(d, arbol("P"
 
 %Nombre preorden
 arbol("S",arbol(a,arbol(u,arbol(l," ","M"),e),arbol(r,c,a)), arbol(d,arbol(o,arbol(" ","P",e),d),arbol(r,arbol(o,z,a),"."))).
-
-%Nombre Postorden
+%Apellido preorden
+arbol("M",arbol(e,arbol(r,c,a),arbol(d,o," ")),arbol(0,arbol(5,1,6),arbol(0,2,"B"))).
+%Nombre y apellido Postorden
 arbol(".",arbol(c,arbol(" ",arbol(u,"S",a),l),arbol(r,"M",e)),arbol(a,arbol("P",arbol(o,a,d)," "), arbol(z,arbol(r,e,d),o))).
-
+arbol(")",arbol(e,arbol(u,"S",a),arbol("M",l," ")),arbol(";",arbol(a,r,c),arbol(" ",d,o))).
 %inorden
 inorden(arbol(Raiz,Hizq,Hder)):- inorden(Hizq),
                                 write(Raiz),
                                 inorden(Hder).
 inorden(H):- write(H).
-
+%mercado
+%pedroza
 %preorden
 preorden(arbol(Raiz,Hizq,Hder)):- write(Raiz), preorden(Hizq), preorden(Hder).
 preorden(H):- write(H).
