3b93f4a843884f4addad52a0351a0d8f3338a394
SaulMerc <saul.mercado9192@alumnos.udg.mx>
2023-11-06 22:20:48 -0600

    [E3.2] Arboles en LISP

---
+++ E3.2_ListasYArboles/arboles.lisp
@@ -0,0 +1,24 @@
+;Representaciones de Árboles mediante listas 
+
+;Listas de cons
+;Estructura: (setf arbol(cons 'raiz(cons 'izq(cons 'der nil))))
+(setf pinturas (cons 'pinturas(cons 'acrilico (cons 'la_joven_de_la_perla (cons 'nighthawks (cons 'acuarela (cons 'jardin_de_aves (cons 'puente_de_londres nil))))) )))
+
+;Listas Anidadas
+;Estructura: (setf arbol '(raiz (izqR (izq der) rder(izq der))))
+(setf arbol '(i (d (e(f (m nil) b(c nil)) a) a(e (f (m nil) b(c nil)) b (e c)))))
+
+(write arbol)
+
+;Funcion que imprime el arbol estilo CONS
+(defun print-cons-tree (tree)
+    (cond
+    ((null tree) nil)
+    (t
+    (format t "~a " (car tree))
+    (print-cons-tree (cdr tree))
+    )
+    )
+)
+
+(print-cons-tree pinturas)
